39f259159dce81b15a8bc9febec9b3a385b05f83
Edward Cheatham <edwardjackc@gmail.com>
2019-05-22 15:15:43 -0600

    Add card appending function
    co-authored-by:Garrett Iannuzzi<garrettiannuzzi@gmail.com>
    > co-authored-by:Djavan Munroe<djavanm24@gmail.com>

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var titleInput = document.querySelector('#top__input--title');
 var bodyInput = document.querySelector('#top__input--body');
 var saveBtn = document.querySelector('#top__input--save');
 var topForm = document.querySelector('#top__form')
+var bottomContainer = document.querySelector('#bottom__container')
 //event listeners 
 titleInput.addEventListener('keypress',validate);
 bodyInput.addEventListener('keypress',validate);
@@ -22,8 +23,25 @@ function clearForm(form) {
   form.reset()
 }
 
-function generateCard() {
-  console.log('Hi');
-  clearForm(topForm);
-  validateInputs(saveBtn,titleInput.value)
-}
+
+
+// function generateCard() {
+//   console.log('Hi');
+//   clearForm(topForm);
+//   validateInputs(saveBtn,titleInput.value)
+// }
+
+function generateCard () {
+bottomContainer.insertAdjacentHTML('afterbegin',`<article class="bottom__article--card">
+  <header class="bottom__header--card">
+    <img class="bottom__icon--card" src="images/star.svg" alt="star__button--inactive"><img class="bottom__icon--card" src="images/delete.svg" alt="delete__button--inactive">
+        </header>
+      <section class="bottom__section--card">
+        <h3 class="bottom__title--card">Idea title</h3>
+        <p class="bottom__paragraph--card">They don’t want us to eat. To succeed you must believe. When you believe, you will succeed. 
+        </section>
+        <footer class="bottom__footer--card">
+          <img class="bottom__icon--card" src="images/upvote.svg" class="bottom__img" id="bottom__img--upvote" alt="upvote__button--inactive"><span class="bottom__span--card">Quality:Swill</span><img class="bottom__icon--card" src="images/downvote.svg" id="bottom__img--downvote" alt="downvote__button--inactive">
+        </footer>
+      </article>`
+)}
